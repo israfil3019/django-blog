b80299c6c11bd79331fce84f82ca7108e05e002f
israfil <israfil3019@gmail.com>
2021-07-25 16:29:05 +0300

    add dynamic main_page

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Blog
 
+
 def main_page(request):
-    return render(request, 'blog/main_page.html')
+    blogs = Blog.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    context = {
+        'blogs': blogs,
+    }
+    return render(request, 'blog/main_page.html', context)
+
 
 def details(request):
     return render(request, 'blog/details.html')
