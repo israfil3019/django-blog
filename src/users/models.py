297a04c19253184024eebdbb1519b45d107d9bf6
israfil <israfil3019@gmail.com>
2021-07-25 11:28:36 +0300

    add path and templates for login

--- src/users/models.py
+++ src/users/models.py
@@ -5,7 +5,7 @@ class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     portfolio = models.URLField(blank=True)
-    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+    user = models.ImageField(upload_to='user', blank=True)
 
     def __str__(self):
         return self.user.username
