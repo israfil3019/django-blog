32c9fb7ecc9e25495668bd256086503e0d6d4a05
israfil <israfil3019@gmail.com>
2021-07-26 12:15:22 +0300

    use details page with  pk

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -5,9 +5,8 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', main_page, name='main_page'),
-    # path("<int:id>/", details, name="details"),
     path("add/", BlogCreate.as_view(), name="add"),
-    path("<int:id>/detail", BlogDetail.as_view(), name="detail"),
+    path("<int:pk>/detail", BlogDetail.as_view(), name="detail"),
     path("<int:id>/update/", BlogUpdate.as_view(), name="update"),
     path("<int:id>/delete/", BlogDelete.as_view(), name="delete"),
 ]
