36882c7902a34ed89e8097f6a637958efd3ab9fd
israfil <israfil3019@gmail.com>
2021-08-23 00:40:40 +0300

    add default registration form

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,24 +1,26 @@
 from django import forms
-from .models import UserProfile
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
+from django.forms import fields
+from .models import Profile
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput) # pasword: ********
-
-
-class UserForm(UserCreationForm):
-
-    class Meta():
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField()  #override;  default required == true 
+    
+    class Meta:
         model = User
-        fields = ('username', 'email')  
-
-
-class UserProfileForm(forms.ModelForm):
-
-    class Meta():
-        model = UserProfile
-        # fields = '__all__'
-        exclude = ('user',)
+        fields = ("username", "email")
+        
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is in use! Use another.")
+        return email
+    
+    # def clean_first_name(self):
+    #     name = self.cleaned_data("first_name")
+    #     if "a" in name:
+    #         raise forms.ValidationError("Your name includes A")
+    #     return name
+        
--- src/users/views.py
+++ src/users/views.py
@@ -1,74 +1,39 @@
-from django.shortcuts import render, redirect, HttpResponse
-from .forms import UserProfileForm, UserForm, LoginForm
-from django.contrib.auth.models import User
-
-# login imports
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 
-
-
-
 def register(request):
-    form_user = UserForm(request.POST or None)
-    form_profile = UserProfileForm(request.POST or None)
-
-    
-    if form_user.is_valid():
-        user = form_user.save()
-        profile = form_profile.save(commit=False)
-        profile.user = user
-
-        if 'profile_pics' in request.FILES:
-            profile.profile_pics = request.FILES['profile_pics']
-
-        profile.save()
-
-        messages.success(request, "Register successful")
-        return redirect('blog:main_page')
-
-    context = {
-        'form_user': form_user,
-        'form_profile': form_profile,
-    }
-
-    return render(request, 'users/register.html', context)
-
-
-@login_required
-def user_logout(request):
-    messages.success(request, "You Logout!")
-    logout(request)
-    return redirect('blog:main_page')
-
-
-def user_login(request):
-    form = LoginForm(request.POST or None)
+    form = RegistrationForm(request.POST or None) 
+    if request.user.is_authenticated:
+        messages.warning(request, "You are already have an account!")
+        return redirect("blog:main_page")
     if form.is_valid():
-        username = form.cleaned_data.get("username")
-        password = form.cleaned_data.get("password")
-
-        user = authenticate(username=username, password=password)
-
-        if user:
-            if user.is_active:
-                messages.success(request, "Login successful")
-                login(request, user)
-                return redirect('blog:main_page')
-            else:
-                messages.error(request, "Account is not active")
-                return render(request, 'users/user_login.html', {"form": form})
-        else:
-            messages.error(request, "Password or Username is wrong!")
-            return render(request, 'users/user_login.html', {"form": form})
-    return render(request, 'users/user_login.html', {"form": form})
-
+        form.save()
+        name = form.cleaned_data["username"]
+        messages.success(request, f"Account created for {name}")
+        return redirect("login")  
+                
+    context = {
+        "form" : form
+    }
+    return render(request, "users/register.html", context)
 
 def profile(request):
-    return render(request, 'users/profile.html')
+    u_form = UserUpdateForm(request.POST or None, instance=request.user)
+    p_form = ProfileUpdateForm(request.POST or None, instance=request.user.profile, files=request.FILES)
+    
+    if u_form.is_valid() and p_form.is_valid():
+        u_form.save()
+        p_form.save()
+        messages.success(request, "Your profile has been updated!")
+        return redirect(request.path)
+    
+    context = {
+        "u_form" : u_form,
+        "p_form" : p_form    
+    }
+    return render(request, "users/profile.html", context)
 
 
 def about(request):
     return render(request, 'users/about.html')
-
