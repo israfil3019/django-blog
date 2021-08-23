b3e18d3cd72321a93808ce5d52b42b099efecfe1
israfil <israfil3019@gmail.com>
2021-08-23 13:22:03 +0300

    change url patterns(slug, function)

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-from .views import BlogCreate, BlogDelete, BlogDetail, BlogList, BlogUpdate
+from .views import BlogList, blog_add, blog_detail, blog_update, blog_delete, like
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'blog'
-
+app_name = "blog"
 urlpatterns = [
     path('', BlogList.as_view(), name='main_page'),
-    path("add/", BlogCreate.as_view(), name="add"),
-    path("<int:pk>/detail", BlogDetail.as_view(), name="detail"),
-    path("<int:id>/update/", BlogUpdate.as_view(), name="update"),
-    path("<int:id>/delete/", BlogDelete.as_view(), name="delete"),
+    path("create/",blog_add, name="add"),
+    path("<str:slug>/",blog_detail, name="detail"),
+    path("<str:slug>/update/",blog_update, name="update"),
+    path("<str:slug>/delete/",blog_delete, name="delete"),
+    path("<str:slug>/like/",like, name="like"),
 ]
 if settings.DEBUG:
      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
