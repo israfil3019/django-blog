1c4832b48cbb255af775fd46b3d73bbe61dc05b2
israfil <israfil3019@gmail.com>
2021-08-23 13:20:57 +0300

    add views(function),upload images(blogs, profile)

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,47 +1,97 @@
-from django.shortcuts import redirect, render
-from django.utils import timezone
-from .models import Blog
-from django.views.generic import DetailView, UpdateView, DeleteView, CreateView, ListView
-from .forms import BlogForm
-from django.urls import reverse_lazy
-from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+from .models import Blog, Like, BlogView, Comment
+from .forms import BlogForm, CommentForm
 from django.contrib.auth.decorators import login_required
-
-
-class BlogCreate(CreateView):
-    model = Blog
-    form_class = BlogForm
-    template_name = "blog/blog_add.html"  # app/blog_form.html  == suffix  '_form'
-    success_url = '/blog/'
-
-class BlogUpdate(UpdateView):
-    model = Blog
-    form_class = BlogForm
-    template_name = "blog/blog_update.html"  # app/blog_form.html  == suffix  '_form'
-    success_url = '/blog/'
-    pk_url_kwarg = 'id'
-
-    def form_valid(self, form):
-        messages.success(self.request, 'Blog updated successfully')
-        return super().form_valid(form)
-
-class BlogDelete(DeleteView):
-    model = Blog
-    form_class = BlogForm
-    template_name = "blog/blog_delete.html"  # app/blog_confirm_delete.html 
-    success_url = '/blog/'
-    pk_url_kwarg = 'id'
-
-    def form_valid(self, form):
-        messages.success(self.request, 'Blog deleted successfully')
-        return super().form_valid(form)
+from django.contrib import messages
+from django.views.generic import ListView
 
 class BlogList(ListView):
     model = Blog
     template_name = "blog/main_page.html"
     context_object_name = 'blogs'
 
-class BlogDetail(DetailView):
-    model = Blog
-    template_name = "blog/blog_detail.html"
-    context_object_name = 'blog'
+@login_required()
+def blog_add(request):
+    # form = BlogForm(request.POST or None, request.FILES or None)
+    form = BlogForm()
+    if request.method == "POST":
+        form = BlogForm(request.POST, request.FILES)
+        # print(request.FILES)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.author = request.user
+            blog.save()
+            messages.success(request, "Blog created successfully!")
+            return redirect("blog:main_page")
+    context = {
+        'form' : form
+    }
+    return render (request, "blog/blog_add.html", context)
+
+def blog_detail(request, slug):
+    form = CommentForm()
+    obj = get_object_or_404(Blog, slug=slug)
+    if request.user.is_authenticated:
+        BlogView.objects.get_or_create(user=request.user, blog=obj)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid:
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.blog = obj
+            comment.save()
+            return redirect("blog:detail", slug=slug)
+    context = {
+        'object' : obj,
+        "form" : form,
+    }
+    return render(request, "blog/blog_detail.html", context)
+
+@login_required()
+def blog_update(request, slug):
+    obj = get_object_or_404(Blog, slug=slug)
+    form = BlogForm(request.POST or None, request.FILES or None, instance=obj)
+    if request.user.id != obj.author.id:
+        # return HttpResponse("You are not authorized!")
+        messages.warning(request, "You are not an author of this blog!")
+        return redirect("blog:main_page")
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Blog updated !!")
+        return redirect("blog:main_page")
+    context = {
+        "object" : obj,
+        "form" : form
+    }
+    return render(request, "blog/blog_update.html", context)
+
+@login_required()
+def blog_delete(request, slug):
+    obj = get_object_or_404(Blog, slug=slug)
+    if request.user.id != obj.author.id:
+        # return HttpResponse("You are not authorized!")
+        messages.warning(request, "You are not an author of this blog!")
+        return redirect("blog:main_page")
+    if request.method == "POST":
+        obj.delete()
+        messages.success(request, "Blog deleted !!")
+        return redirect("blog:main_page")
+    
+    context = {
+        "object" : obj
+    }
+    return render(request, "blog/blog_delete.html", context)
+
+@login_required()
+def like(request, slug):
+    if request.method == "POST":
+        obj = get_object_or_404(Blog, slug=slug)
+        like_qs = Like.objects.filter(user=request.user, blog=obj)
+        if like_qs.exists():
+            like_qs[0].delete()
+        else:
+            Like.objects.create(user=request.user, blog=obj)
+        return redirect("blog:detail", slug=slug)
+    return redirect("blog:detail", slug=slug)
+
+
