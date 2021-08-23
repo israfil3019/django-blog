1d3760588603fb67118bc611dfd17db7637c80fe
israfil <israfil3019@gmail.com>
2021-08-23 12:53:07 +0300

    change form, add models to admin

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,9 +1,24 @@
 from django import forms
-from .models import Blog
-
+from .models import Blog, Comment, Category
 
 class BlogForm(forms.ModelForm):
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Select Category")
+
 
     class Meta:
         model = Blog
-        fields = ["author", "title", "text", "img"]
+        widgets = {
+            'text': forms.Textarea(attrs={'rows':'5', 'cols': '40'}),
+        }
+        fields = (
+            'title',
+            'text',
+            'image',
+            'category',
+        )
+        
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
+        
